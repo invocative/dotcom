2840cbfcfd4a1a9c6c38f50c123036b268271b24
Yuuki Wesp <ls-micro@ya.ru>
2019-11-01 03:17:56 +0300

    stage

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    entry: "./src/scripts/@boom.js",
+    output: {
+        path: path.join(__dirname, "/public"),
+        filename: "@nya.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: '.',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '@nya.css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
+};
